a60ddf06a847ac8f9cd9b54fae7a128f7ee83980
MCshin00 <sinminc7@gmail.com>
2025-04-18 17:37:39 +0900

    feat: jwtUtil 구현 (#2)
    jwt 토큰 생성, 추출, 인증 담당 util 클래스 구현

---
+++ src/main/java/com/careerthon/assignment/util/JwtUtil.java
@@ -0,0 +1,86 @@
+package com.careerthon.assignment.util;
+
+import com.careerthon.assignment.exception.CustomJwtException;
+import com.careerthon.assignment.exception.JwtExceptionMessage;
+import com.careerthon.assignment.model.entity.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Slf4j(topic="jwtUtil")
+@Component
+public class JwtUtil {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+    private Key key;
+
+    @Value("${service.jwt.expire-time}")
+    private Long expireTime;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(Long userId, String username, UserRole role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX + Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .claim("username", username)
+                .claim("role", role)
+                .setExpiration(new Date(date.getTime() + expireTime))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm) // Key 객체를 사용
+                .compact();
+    }
+
+    public String extractToken(HttpServletRequest request) {
+        String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith(BEARER_PREFIX)) {
+            return authHeader.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+        } catch (SecurityException | MalformedJwtException | SignatureException | ExpiredJwtException |
+                 UnsupportedJwtException | IllegalArgumentException e) {
+
+            log.warn(JwtExceptionMessage.INVALID_TOKEN.getMessage());
+            throw new CustomJwtException(e.getMessage());
+
+        }
+    }
+}
