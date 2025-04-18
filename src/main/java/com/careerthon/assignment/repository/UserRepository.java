e90f9c44965fded845c57a88fc6fe673a07d1819
MCshin00 <sinminc7@gmail.com>
2025-04-18 16:41:53 +0900

    feat: UserRepository 구현 (#1)

---
+++ src/main/java/com/careerthon/assignment/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.careerthon.assignment.repository;
+
+import com.careerthon.assignment.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
